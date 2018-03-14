9bce26704a9a727413291708aa84ee623ad21e6d
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2018-03-14 22:30:27 +0000

    Code cleanup. Added class docstring

--- Photoshop/fileConverter.py
+++ Photoshop/fileConverter.py
@@ -2,13 +2,34 @@ from comtypes.client import CreateObject
 import os
 
 class Converter():
-    '''This class converts files to different formats using Photoshop'''
-    def __init__(self):
+    '''
+    Convert files to different formats using Photoshop
+    
+    Class usage:
+        Converter(['png', 'file1.psd', 'file2.psd', 'file3.psd'])
+        
+    Cmd execution:
+        fileConverter.py "png" "file1.psd" "file2.psd" "file3.psd"
+    '''
+    def __init__(self, arguments):
         self.ps = CreateObject('Photoshop.Application')
         self.ps.Preferences.RulerUnits = 1
+        self.format = ''
+        self.files = []
+        
+        if arguments:
+            self.format = arguments[0]
+            self.files = arguments[1:]
+        else:
+            print('No arguments have been passed!')
+        
+        if self.format.lower() == 'png':
+            self.convertToPNG()
+        
+        self.quit()
     
-    def convertToPNG(self, files):
-        for file in files:
+    def convertToPNG(self):
+        for file in self.files:
             self.ps.Application.Open(file)
             outFile = os.path.splitext(file)[0] + '.png'
             
@@ -19,12 +40,13 @@ class Converter():
             
             self.ps.ActiveDocument.SaveAs(outFile, options, saveAsCopy)
             self.ps.ActiveDocument.Close()
-
+    
+    def quit(self):
         #Close Photoshop if no documents are open
         if not self.ps.Documents.Count: self.ps.Application.Quit()
         
 if __name__ == '__main__':
     import sys
     
-    arguments = sys.argv[1:]
-    pngConv = Converter().convertToPNG(arguments)
+    Converter(sys.argv[1:])
+    
